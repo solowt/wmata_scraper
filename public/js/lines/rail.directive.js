d245967e0abb58ed5723c7b7f0871dc125dfe5b7
Thomas Solow <thomassolow@gmail.com>
2016-01-15 09:01:43 -0500

    stable...done...pushable to heroku

--- public/js/lines/rail.directive.js
+++ public/js/lines/rail.directive.js
@@ -23,8 +23,13 @@
       link: function(scope, elm){
         console.log('In rail directive.')
         scope.$watch("times.join('').hashCode()", function() {
+          console.log(scope.str)
           setTimeout(function(){
-            $(".trainwrapper").remove()
+            if (scope.str == "top"){
+              $(".topTrain").remove()
+            } else if (scope.str =="bot"){
+              $(".botTrain").remove()
+            }
             scope.drawTrains()
           },30)
         });
@@ -42,6 +47,11 @@
             this.ms = this.time*60000;
           }
           this.$el = $(scope.trainTemplate);
+          if (scope.str=="top"){
+            this.$el.addClass("topTrain");
+          }else if (scope.str =="bot"){
+            this.$el.addClass("botTrain")
+          }
           this.offset = 0;
           // self.trains.push(this);
         }
@@ -56,20 +66,35 @@
         }
         scope.Train.prototype.append = function(){
           this.$el.appendTo($("body"));
-          if (this.status == "ARR"){
+          if (this.status == "ARR" && scope.str=="top"){
             this.$el.offset({top:this.y+30, left:this.x-this.offset});
-          }else if (this.status == "BRD"){
+          }else if (this.status == "BRD" && scope.str=="top"){
             this.$el.offset({top:this.y+30, left:this.x-this.offset});
-          }else {
+          }else if(scope.str=="top") {
             this.$el.offset({top:this.y+30, left:this.x-this.offset});
+          }
+          if (scope.str == "bot"){
+            if (this.status == "ARR"){
+              this.$el.offset({top:this.y+30, left:this.x+this.offset});
+            }else if (this.status == "BRD"){
+              this.$el.offset({top:this.y+30, left:this.x+this.offset});
+            }else{
+              this.$el.offset({top:this.y+30, left:this.x+this.offset});
+
+            }
           }
         }
         scope.Train.prototype.animate = function(){
           // console.log(s);
-          if (this.status == "ARR") {
+          if (this.status == "ARR"  && scope.str=="top") {
             this.$el.animate({left: this.x}, 30000);
-          } else {
-            this.$el.animate({left:this.x}, this.ms/2)
+          } else if (scope.str=="top") {
+            this.$el.animate({left:this.x}, this.ms/2);
+          }
+          if (this.status == "ARR" && scope.str =="bot"){
+            this.$el.animate({left: this.x}, 30000);
+          }else if (scope.str=="bot"){
+            this.$el.animate({left:this.x}, this.ms/2);
           }
         }
         scope.Train.prototype.delete = function(){
@@ -88,30 +113,60 @@
           }
         }
         scope.drawTrains = function() {
-          for (var i = 0; i<scope.times.length; i++){
+          if (scope.str=="top"){
+            for (var i = 0; i<scope.times.length; i++){
 
-            if (scope.str=="top" && scope.times[i] == "BRD" && $("#"+scope.str+(i)).offset()){
-              var aTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
-              aTrain.getOffset();
-              aTrain.append();
+              if (scope.times[i] == "BRD" && $("#"+scope.str+(i)).offset()){
+                var aTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
+                aTrain.getOffset();
+                aTrain.append();
 
-            }else if (scope.str=="top" && scope.times[i] == "ARR" && $("#"+scope.str+(i)).offset()){
-              var bTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
-              bTrain.getOffset();
-              bTrain.append();
-              bTrain.animate();
+              }else if (scope.times[i] == "ARR" && $("#"+scope.str+(i)).offset()){
+                var bTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
+                bTrain.getOffset();
+                bTrain.append();
+                bTrain.animate();
 
-            }else if(scope.str=="top" && scope.times[i]=="N/A" && scope.times[i]!="BRD" && scope.times[i]!="ARR"){
-              if (scope.times[i+1] != "BRD" && scope.times[i+1] != "ARR" && parseInt(scope.times[i+1]) <=3){
-                var vTrain = new scope.Train($("#"+scope.str+(i+1)).offset(), scope.times[i]);
-                vTrain.getOffset();
-                vTrain.append();
-                // vTrain.animsate();
+              }else if(scope.times[i]=="N/A" && scope.times[i]!="BRD" && scope.times[i]!="ARR"){
+                if (scope.times[i+1] != "BRD" && scope.times[i+1] != "ARR" && parseInt(scope.times[i+1]) <=3){
+                  var vTrain = new scope.Train($("#"+scope.str+(i+1)).offset(), scope.times[i]);
+                  vTrain.getOffset();
+                  vTrain.append();
+                  // vTrain.animate();
+                }
+              }else if (scope.times[i+1] != "ARR" && scope.times[i+1] != "BRD" && parseInt(scope.times[i])-scope.convert(scope.times[i+1]) > 0) {
+                var cTrain = new scope.Train($("#"+scope.str+(i+1)).offset(), scope.times[i]);
+                cTrain.getOffset();
+                cTrain.append();
               }
-            }else if (scope.str=="top" && scope.times[i+1] != "ARR" && parseInt(scope.times[i])-scope.convert(scope.times[i+1]) > 0) {
-              var cTrain = new scope.Train($("#"+scope.str+(i+1)).offset(), scope.times[i]);
-              cTrain.getOffset();
-              cTrain.append();
+            }
+          }else if (scope.str=="bot"){
+            for (var i = 0; i<scope.times.length; i++){
+
+              if(scope.times[i] == "BRD" && $("#"+scope.str+(i)).offset()){
+                var lTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
+                lTrain.getOffset();
+                lTrain.append();
+              }
+              else if(scope.times[i] == "ARR" && $("#"+scope.str+(i)).offset()){
+                var kTrain = new scope.Train($("#"+scope.str+(i)).offset(), scope.times[i]);
+                kTrain.getOffset();
+                kTrain.append();
+                kTrain.animate();
+              }
+              else if(scope.times[i]=="N/A" && scope.times[i]!="BRD" && scope.times[i]!="ARR"){
+                if (scope.times[i-1] != "BRD" && scope.times[i-1] != "ARR" && parseInt(scope.times[i-1]) <=3){
+                  var pTrain = new scope.Train($("#"+scope.str+(i-1)).offset(), scope.times[i]);
+                  pTrain.getOffset();
+                  pTrain.append();
+                  // vTrain.animate();
+                }
+              }
+              else if (scope.times[i-1] != "ARR" && scope.times[i-1] != "BRD" && parseInt(scope.times[i])-scope.convert(scope.times[i-1]) > 0) {
+                var yTrain = new scope.Train($("#"+scope.str+(i-  1)).offset(), scope.times[i]);
+                yTrain.getOffset();
+                yTrain.append();
+              }
             }
           }
         }
